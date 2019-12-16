a5cee47a700677282bce35d33a6d9a61bde44201
emmettsrm <emmettsrm@gmail.com>
2019-12-16 16:50:41 +0200

    set state on button click/tweet typed

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React from 'react';
+// import { PostTweet } from './api';
 
 class CreateTweet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: 'becca',
+            tweet: '',
+        }
+    }
+    onTweetChange(event) {
+        this.setState({ tweet: event.target.value});
+    }
+    
+    onSendTweet() {
+        const {tweet} = this.state;
+        const { onTweetPosted } = this.props
+        console.log(tweet)
+    }
+    
+    
     render(){
         return (
             <div className="createTweet">
-                <textarea className="writeTweet" type="text" placeholder="What you have in mind..."></textarea>
-                <button>Tweet</button>
+                <textarea className="writeTweet" onChange={(event)=>this.onTweetChange(event)}type="text" placeholder="What you have in mind..." ></textarea>
+                <button onClick={() => this.onSendTweet()}>Tweet</button>
             </div>
-        )
-    }
-}
+        )}
 
-export default CreateTweet
+}
+export default CreateTweet;
